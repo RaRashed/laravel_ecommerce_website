572622c0f0d6ec872cd10a49b09b87583c1bf1c2
RaRashed <rnrashedrn@gmail.com>
2022-07-31 04:40:07 +0600

    seed

--- database/seeds/UserSeeder.php
+++ database/seeds/UserSeeder.php
@@ -15,9 +15,9 @@ class UserSeeder extends Seeder
         User::create([
             'first_name' =>"Rashed",
             'last_name' =>"Rashed",
-            'username' =>"RaaRashed",
-            'phone_no' =>"12345678123",
-            'email' => "rashed@gmail.com",
+            'username' =>"RaaaaRashed",
+            'phone_no' =>"1234567812",
+            'email' => "rarashed@gmail.com",
             'password' => Hash::make("12345678"),
 
             'street_address' =>"mirpur",
